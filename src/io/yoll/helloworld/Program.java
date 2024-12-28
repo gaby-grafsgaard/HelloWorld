154f790a93adad58107f1e8dfb4d9f5afdc83f83
gaby-grafsgaard <gabyinhawaii@gmail.com>
2024-12-27 15:15:48 -1000

    Added a method to help with figuring out whether I am camping or not

--- src/io/yoll/helloworld/Program.java
+++ src/io/yoll/helloworld/Program.java
@@ -4,7 +4,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hello from Yoll Application");
 	}
-
+public static boolean CanGoCamping (int  temperature) {
+	if (temperature>20) {
+		return true;
+	}else {
+		return false;
+	}
+}
 }
